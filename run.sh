8934d3f53dbe9b6bfe4694363c767eaf8d35d0aa
Jade <it56070021@it.kmitl.ac.th>
2016-10-14 03:16:59 -0700

    nonegraph

--- run.sh
+++ run.sh
@@ -16,3 +16,13 @@ do
 	sudo python hedera.py gff $file
 	sleep 6
 done
+
+for file in traffic/*
+do
+        killall -9 python2.7
+        ~/pox/pox.py controllers.riplpox --topo=ft,4 --routing=hashed --mode=reactive &
+        sleep 2
+        sudo python noneblock.py none $file
+        sleep 6
+done
+
